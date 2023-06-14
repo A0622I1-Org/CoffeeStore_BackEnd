38250390215829324507435faac0d98d90114429
minhcuong2102 <cuong21022003@gmail.com>
2023-06-14 19:18:55 +0700

    update

--- spring_boot/src/main/java/com/codegym/man_hinh_ban_hang/dto/BillChargingListDTO.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.man_hinh_ban_hang.dto;
-
-public interface BillChargingListDTO {
-    public Integer getBillId();
-    public Integer getUserId();
-    public Integer getTableId();
-    public Double getSum();
-}
