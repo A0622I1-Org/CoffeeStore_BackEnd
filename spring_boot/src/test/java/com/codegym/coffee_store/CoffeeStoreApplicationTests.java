9915d446239ae4ecb17a9a2a3125aa080744f1b2
minhcuong2102 <cuong21022003@gmail.com>
2023-06-16 18:26:25 +0700

    update

--- spring_boot/src/test/java/com/codegym/coffee_store/CoffeeStoreApplicationTests.java
+++ spring_boot/src/test/java/com/codegym/coffee_store/CoffeeStoreApplicationTests.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:spring_boot/src/test/java/com/codegym/backend/CoffeeStoreApplicationTests.java
-package com.codegym.backend;
-========
 package com.codegym.coffee_store;
->>>>>>>> origin/develop1:spring_boot/src/test/java/com/codegym/coffee_store/CoffeeStoreApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
