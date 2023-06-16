6e85c349ef8ead4da151b677ec4746bc817cc0b6
tulg <tulg@mcrew-tech.com>
2023-06-16 19:45:16 +0700

    no message

--- spring_boot/src/main/java/com/codegym/backend/repository/UserRepository.java
+++ spring_boot/src/main/java/com/codegym/backend/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.codegym.backend.repository;
 
-import com.codegym.backend.dto.UserFindIdDTO;
 import com.codegym.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,14 +11,6 @@ import javax.transaction.Transactional;
 @Transactional
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-
-
-    @Query(value = "select user.id = ?1, user.name =?2, user.address =?3, user.phone_number=?4, user.birthday=?5, user.gender=?6," +
-            " user.salary=?7, user.position_id=?8, user.account_id=?9,user.img_url=?10 from user where user.id = ?1", nativeQuery = true)
-    UserFindIdDTO findById(int id);
-
-
     @Modifying
     @Query(value = "update user as u set u.name = ?1, u.address = ?2, u.phone_number = ?3, u.birthday = ?4, u.gender = ?5," +
             " u.salary = ?6, u.account_id = ?7, u.img_url = ?8, u.position_id = ?9 where u.id = ?10", nativeQuery = true)
