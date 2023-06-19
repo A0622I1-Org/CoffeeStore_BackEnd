1484f8a6e2718c29aef7a2280e06eafda55ef838
tulg <tulg@mcrew-tech.com>
2023-06-19 19:37:57 +0700

    fix show man hinh menu

---
+++ spring_boot/src/main/java/com/codegym/backend/service/Impl/ServiceTypeService.java
@@ -0,0 +1,18 @@
+package com.codegym.backend.service.Impl;
+
+import com.codegym.backend.model.ServiceType;
+import com.codegym.backend.repository.IServiceTypeRepository;
+import com.codegym.backend.service.IServiceTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ServiceTypeService implements IServiceTypeService {
+    @Autowired
+    IServiceTypeRepository iServiceTypeRepository;
+    @Override
+    public List<ServiceType> findAllServiceType() {
+        return iServiceTypeRepository.findAllServiceType();
+    }
+}
